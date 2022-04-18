01b91b436620ffd4dfe52e906d9867ce3e368c0c
Mohan Pradhan <manojpradhan628@gmail.com>
2022-04-18 17:19:12 +0545

    Revert "crud implemented for users"
    This reverts commit 1d294557da5f006caf75f1d1691e0942a752e540.
    
    revert commit

--- app.js
+++ app.js
@@ -2,29 +2,10 @@ require("dotenv/config");
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5001;
-const user = require("./routes/usersRoutes");
-const mongoose = require("mongoose");
-
-//Body Parser
-app.use(express.json());
-
-//MangoDB connection
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const connection = mongoose.connection;
-connection.once("open", () => {
-  console.log("MongoDB connection established sucessfully");
-});
 
 app.get("/", (req, res) => {
   res.send("Hello, Optimum Futurists!!!");
 });
-
-app.use("/user", user);
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
