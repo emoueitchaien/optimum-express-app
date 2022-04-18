176f8c5ee80248e7cada5faff308c4842ecbf84b
Mohan Pradhan <manojpradhan628@gmail.com>
2022-04-18 10:19:06 +0545

    project setup

---
+++ app.js
@@ -0,0 +1,13 @@
+require("dotenv/config");
+const { clear } = require("console");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.get("/", (req, res) => {
+  res.send("Hello, Optimum Futurists!!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
