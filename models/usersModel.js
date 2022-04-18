1d294557da5f006caf75f1d1691e0942a752e540
Mohan Pradhan <manojpradhan628@gmail.com>
2022-04-18 17:15:51 +0545

    crud implemented for users

---
+++ models/usersModel.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    Name: { type: String, required: true },
+    Address: { type: String, required: true },
+    Contact: { type: Number, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const user = mongoose.model("user", userSchema);
+
+module.exports = user;
