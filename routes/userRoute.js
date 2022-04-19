047318d6d7243d42b314c3c4ad292dc409423a8d
Mohan Pradhan <manojpradhan628@gmail.com>
2022-04-19 13:06:04 +0545

    updated routes url

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,10 +4,12 @@ var router = express.Router();
 //require controller modules
 var userController = require("../controllers/userController");
 
-router.get("/", userController.getUsers);
-router.post("/add", userController.addUser);
-router.get("/:id", userController.getUserById);
-router.put("/update/:id", userController.updateUser);
-router.delete("/delete/:id", userController.deleteUser);
+router.route("/").get(userController.getUsers).post(userController.addUser);
+
+router
+  .route("/:id")
+  .get(userController.getUserById)
+  .put(userController.updateUser)
+  .delete(userController.deleteUser);
 
 module.exports = router;
