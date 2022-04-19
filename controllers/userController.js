2bf638cb79505eba610438cbecb3b2d4aefac6bc
Mohan Pradhan <manojpradhan628@gmail.com>
2022-04-20 01:43:25 +0545

    promise changed to async await with try catch

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,62 +1,62 @@
 import user from "../models/userModel.js";
 
 const userController = {
-  getUsers: (req, res) => {
-    user
-      .find()
-      .then((user) => res.json({ data: user, status: 200 }))
-      .catch((err) => res.status(400).json("Error:" + err));
+  getUsers: async (req, res) => {
+    try {
+      const userRes = await user.find();
+      res.status(200).json({ data: userRes, status: 200 });
+    } catch (err) {
+      res.status(400).json({ Error: err });
+    }
   },
 
-  addUser: (req, res) => {
+  addUser: async (req, res) => {
     const newUser = new user({
       Name: req.body.Name,
       Email: req.body.Email,
       Address: req.body.Address,
       Contact: Number(req.body.Contact),
     });
-    newUser
-      .save()
-      .then(() => res.json("User Added"))
-      .catch((err) => res.status(400).json("Error:" + err));
+    try {
+      const userRes = await newUser.save();
+      res.status(200).json({ "User Added": userRes });
+    } catch (err) {
+      res.status(400).json({ Error: err });
+    }
   },
 
-  getUserById: (req, res) => {
-    user
-      .findById(req.params.id)
-      .then((User) => res.json(User))
-      .catch((err) => res.status(400).json("Error:" + err));
+  getUserById: async (req, res) => {
+    try {
+      const userRes = await user.findById(req.params.id);
+      res.status(200).json({ "User with Id": userRes });
+    } catch (err) {
+      res.status(400).json({ Error: err });
+    }
   },
 
-  deleteUser: (req, res) => {
-    user
-      .findByIdAndDelete(req.params.id)
-      .then((user) => res.json(`${user} deleted`))
-      .catch((err) => res.status(400).json("Error:" + err));
+  updateUser: async (req, res) => {
+    try {
+      const userRes = await user.findById(req.params.id);
+
+      userRes.Name = req.body.Name;
+      userRes.Email = req.body.Email;
+      userRes.Address = req.body.Address;
+      userRes.Contact = Number(req.body.Contact);
+
+      const update = await userRes.save();
+      res.status(200).json("User Updated");
+    } catch (err) {
+      res.status(400).json("Error:" + err);
+    }
   },
 
-  updateUser: (req, res) => {
-    user
-      .findById(req.params.id)
-      .then((user) => {
-        user.Name = req.body.Name;
-        user.Email = req.body.Email;
-        user.Address = req.body.Address;
-        user.Contact = Number(req.body.Contact);
-
-        user
-          .save()
-          .then(() => res.json("User Updated"))
-          .catch((err) => res.status(400).json("Error:" + err));
-      })
-      .catch((err) => res.status(400).json("Error:" + err));
-  },
-
-  deleteUser: (req, res) => {
-    user
-      .findByIdAndDelete(req.params.id)
-      .then((user) => res.json(`${user} deleted`))
-      .catch((err) => res.status(400).json("Error:" + err));
+  deleteUser: async (req, res) => {
+    try {
+      const userRes = await user.findByIdAndDelete(req.params.id);
+      res.json({ "User Deleted": userRes });
+    } catch (err) {
+      res.status(400).json("Error:" + err);
+    }
   },
 };
 
