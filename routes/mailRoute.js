c5f9a9ac8f9cbdba4436cacfc16bd80e5cb2b95c
Mohan Pradhan <manojpradhan628@gmail.com>
2022-04-21 16:00:00 +0545

    minor update to mailHandler middleware

--- routes/mailRoute.js
+++ routes/mailRoute.js
@@ -1,10 +1,10 @@
 import express from "express";
-import mail from "../middlewares/mailHandler.js";
+import mailHandler from "../middlewares/mailHandler.js";
 
 let router = express.Router();
 
 import mailController from "../controllers/mailController.js";
 
-router.route("/").post(mail, mailController.sendMail);
+router.route("/").post(mailHandler, mailController.sendMail);
 
 export default router;
