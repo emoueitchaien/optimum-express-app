898bdf05cd8c90fb6595b475425b876f52ffb2d6
Mohan Pradhan <manojpradhan628@gmail.com>
2022-04-29 22:00:41 +0545

    removed comment

--- src/routes/projectRoute.js
+++ src/routes/projectRoute.js
@@ -2,9 +2,6 @@ import express from "express";
 var router = express.Router();
 
 import projectController from "../controllers/projectController.js";
-// import requireLogin from "../middlewares/requireLogin.js";
-
-// router.use(requireLogin);
 
 router.route("/").get(projectController.fetchProjects);
 
